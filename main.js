d498e6df14a79266f421a721c8217254b8c6dc93
ColeAnderson7278 <canderson@basecampcodingacademy.org>
2019-01-25 16:16:24 -0600

    Added getMonthlyPayment function
    This function, and the way everything is set up
    for now, is a bit messy; however, everything
    does seem to work. Now I need to make the
    others to find the other data. Then organize
    these functions.

--- main.js
+++ main.js
@@ -1,6 +1,33 @@
 function checkForSubmitButton() {
     var button = document.querySelector("#formSubmitButton");
-    button.addEventListener("click", function() {});
+    button.addEventListener("click", function() {
+        getTotalPayment();
+    });
 }
 
 checkForSubmitButton();
+
+function getTotalPayment() {
+    var homePrice = document.querySelector("#homePriceInput").value;
+    var downPaymentPrice = document.querySelector("#downPaymentPriceInput")
+        .value;
+    var downPaymentPercent = document.querySelector("#downPaymentPercentInput")
+        .value;
+    var amountOfMonths = document.querySelector("#loanProgramSelection").value;
+    var interestRate = document.querySelector("#interestRateInput").value;
+    document.querySelector("#totalPaymentHolder").innerText = getMonthlyPayment(
+        interestRate,
+        amountOfMonths,
+        homePrice
+    );
+}
+
+function getMonthlyPayment(interestRate, amountOfMonths, homePrice) {
+    var rate = interestRate * 0.001;
+    var nper = amountOfMonths * 12;
+    var pv = homePrice;
+    var monthlyPayment =
+        pv *
+        ((rate * Math.pow(1 + rate, nper)) / (Math.pow(1 + rate, nper) - 1));
+    return monthlyPayment;
+}
