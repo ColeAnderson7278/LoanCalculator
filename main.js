a0f9bf8182ba09990f658307ead78b793dc65750
ColeAnderson7278 <canderson@basecampcodingacademy.org>
2019-01-30 15:37:34 -0600

    Created set and calculate for all variables.
    I went through and made a set and calculate
    for all the different variables. These include
    the PMI, taxes, etc.. These help to find the
    values, set the values to global varibles, and
    then used these variables to show them to the
    user.

--- main.js
+++ main.js
@@ -1,33 +1,73 @@
+const homePrice = document.querySelector("#homePriceInput").value;
+const amountOfMonths = document.querySelector("#loanProgramSelection").value;
+const interestRate = document.querySelector("#interestRateInput").value;
+var pAndI;
+var PMI;
+var insurance;
+var tax;
+var monthlyPayment;
+
 function checkForSubmitButton() {
     var button = document.querySelector("#formSubmitButton");
     button.addEventListener("click", function() {
-        getTotalPayment();
+        setPAndIPayment();
+        setPMIPayment();
+        setInsurancePayment();
+        setTaxPayment();
+        setMonthlyPayment();
     });
 }
 
 checkForSubmitButton();
 
-function getTotalPayment() {
-    var homePrice = document.querySelector("#homePriceInput").value;
-    var downPaymentPrice = document.querySelector("#downPaymentPriceInput")
-        .value;
-    var downPaymentPercent = document.querySelector("#downPaymentPercentInput")
-        .value;
-    var amountOfMonths = document.querySelector("#loanProgramSelection").value;
-    var interestRate = document.querySelector("#interestRateInput").value;
-    document.querySelector("#totalPaymentHolder").innerText = getMonthlyPayment(
-        interestRate,
-        amountOfMonths,
-        homePrice
-    );
+function setPAndIPayment() {
+    calculatePAndIPayment(interestRate, amountOfMonths, homePrice);
+    document.querySelector("#piHolder").innerText = "$" + pAndI;
 }
 
-function getMonthlyPayment(interestRate, amountOfMonths, homePrice) {
+function calculatePAndIPayment(interestRate, amountOfMonths, homePrice) {
     var rate = interestRate * 0.001;
     var nper = amountOfMonths * 12;
     var pv = homePrice;
-    var monthlyPayment =
+    var PandIPayment =
         pv *
         ((rate * Math.pow(1 + rate, nper)) / (Math.pow(1 + rate, nper) - 1));
-    return monthlyPayment;
+    pAndI = Math.round(PandIPayment);
+}
+
+function setPMIPayment() {
+    calculatePMIPayment(homePrice);
+    document.querySelector("#pmiHolder").innerText = "$" + PMI;
+}
+
+function calculatePMIPayment(homePrice) {
+    PMI = Math.round((homePrice * 0.01) / 12);
+}
+
+function setInsurancePayment() {
+    calculateInsurancePayment(homePrice);
+    document.querySelector("#insuranceHolder").innerText = "$" + insurance;
+}
+
+function calculateInsurancePayment(homePrice) {
+    insurance = Math.round((homePrice / 100000) * 35);
+}
+
+function setTaxPayment() {
+    calculateTaxPayment(homePrice);
+    document.querySelector("#taxHolder").innerText = "$" + tax;
+}
+
+function calculateTaxPayment(homePrice) {
+    tax = Math.round((homePrice * 0.015) / 12);
+}
+
+function setMonthlyPayment() {
+    calculateMonthlyPayment(pAndI);
+    document.querySelector("#monthlyPaymentHolder").innerText =
+        "$" + monthlyPayment;
+}
+
+function calculateMonthlyPayment(pAndI) {
+    monthlyPayment = pAndI + PMI + insurance + tax;
 }
